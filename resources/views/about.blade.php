3f6655f59f0b4f8290b2a089cf7fa42a76ef2e20
REJIL K P <rejil2009@gmail.com>
2020-12-02 21:21:46 +0530

    First Laravel Sample Project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends("layouts.common");
+@section("title","About");
+@section("documentation-content")
+Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
+@endsection
+{{$message}}
+ID:{{$id}}
